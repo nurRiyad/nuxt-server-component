a129750b61a65317365075fe92fa3b8dce2d80db
nurRiyad <asadnurriyad@gmail.com>
2024-12-04 21:54:44 +0600

    Update server component
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/post">Post</NuxtLink>
+    </div>
+    <NuxtPage/>
   </div>
 </template>
---
+++ pages/index.server.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+
+const { data } = useFetch<Array<Record<string,string>>>('https://jsonplaceholder.typicode.com/posts');
+const searchText = ref('')
+
+const filterEdData = computed(() => {
+  if (!searchText.value) return data.value
+  return data.value?.filter((item: any) => item.title.includes(searchText.value))
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Normal Component(Search Work)</h1>
+    <input type="text" v-model="searchText" placeholder="Search" />
+    <pre>{{ filterEdData }}</pre>
+  </div>
+</template>
